d5c5985f9f137cac0fe93761a33c41876f3ee9d3
Maryellen Marino <74571866+maryellenmarino@users.noreply.github.com>
2024-03-23 21:53:52 -0400

    update naming and namespace

--- Plugin.cs
+++ Plugin.cs
@@ -3,7 +3,7 @@ using BepInEx;
 using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
-namespace Template;
+namespace LCOS.RandomSprint;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
